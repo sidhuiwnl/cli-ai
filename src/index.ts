1fe2bd0183a7450ad88615bb60f268e2d40a8b7a
Sidharth <sidharthinfernal@gmail.com>
2024-11-06 23:43:31 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
+import { red,green } from "kolorist";
+
 import { intro,outro } from "@clack/prompts";
 import { program } from "commander";
 
 import  { execa } from "execa"
 
-intro("Welcome to aicli!!!")
+
 
 program
     .option('--first')
     .option('-s,--seperator  <char>');
 
 
-program.command('message')
+program.command("message")
     .description("to view the all the changes in the project")
     .action(async() =>{
       const  { stdout } = await execa('git',['diff']);
-      console.log(stdout)
+      
     })
 program.parse();
 
 
-outro('you are all set')
